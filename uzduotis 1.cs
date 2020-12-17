93b9dc3edf4891fb5e97b260b86706e766ea89a6
deimantassluk <76051556+deimantassluk@users.noreply.github.com>
2020-12-17 15:55:38 +0200

    Add files via upload

---
+++ uzduotis 1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace Programuoju1
+{
+    class Program1
+    {
+        static void Main(String[] args) 
+
+        {
+            var word = Console.ReadLine();
+            var letter1 = word[0];
+            Console.WriteLine(letter1 + word + letter1);
+            
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
